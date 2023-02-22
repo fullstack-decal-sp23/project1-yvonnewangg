1081ff5477ad5c15abe50c1b14f2928a9792f0d1
Yvonne Wang <wangyvonne@berkeley.edu>
2023-02-22 00:59:41 -0800

    finished first personal website

---
+++ script.js
@@ -0,0 +1,10 @@
+$(document.ready(function(){
+    $(window).scroll(function(){
+    var typed = new Typed(".typing", {
+        strings: ["Software Engineer", "Developer", "Designer", "Student"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+})
+}));
